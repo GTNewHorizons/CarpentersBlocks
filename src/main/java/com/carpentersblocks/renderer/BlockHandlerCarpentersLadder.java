ae6415ecd68fcd04df51baba452ac7f378047daa
GitHub GTNH Actions <unknown>
2024-01-08 01:03:48 +0000

    spotlessApply

--- src/main/java/com/carpentersblocks/renderer/BlockHandlerCarpentersLadder.java
+++ src/main/java/com/carpentersblocks/renderer/BlockHandlerCarpentersLadder.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import com.carpentersblocks.data.Ladder;
